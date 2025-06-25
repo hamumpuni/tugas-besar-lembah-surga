7f1bf34332bb536056ce3d99c5341008384602d0
hamumpuni <intanhamumpuni@gmail.com>
2025-06-25 20:54:09 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// menu open close
+let menu = document.querySelector(".menu-icon");
+
+menu.onclik = () => {
+  menu.classList.toggle("move");
+};
+<button onclick="document.getElementById('booking-form').scrollIntoView({ behavior: 'smooth' });">
+  Book Now
+</button>
